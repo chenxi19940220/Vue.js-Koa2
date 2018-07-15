475f79001144116308e6b10d1ace222f7b38cf0f
chenxi19940220 <229168514@qq.com>
2018-07-15 11:57:55 +0800

    完成 首页布局 首页交互开发

---
+++ src/components/swiper/swiperText.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide class="text">
+        <div></div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data () {
+    return {
+      swiperOption: {
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        freeMode: true,
+        mousewheel: true
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style>
+.swiper {
+  height: 300px;
+  overflow: hidden;
+}
+.text {
+  font-size: 18px;
+  text-align: left;
+  padding: 30px;
+  height: auto;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+</style>
